3b8b96ce553a08718d2ac484f215d5e5b5d269ac
fernando <nanocatdemen@gmail.com>
2015-10-16 20:55:38 -0300

    Refactor more code. Squanch and create code skeleton

--- get.py
+++ get.py
@@ -1,90 +1,31 @@
-import requests as _r
-import xml.etree.ElementTree as _et
-import xml.dom.minidom
-import sys
+from request.find_request import FindRequest
+from request.present_request import PresentRequest
 
-# Correr el script desde la linea de comandos, poniendo como parametro al autor.
-# Por ejemplo: python get.py "Cortazar, Julio"
+author = 'Cortazar, Julio'  # sys.argv[1]
 
-autor = sys.argv[1]
 url = 'http://www.bncatalogo.cl/X'
-output = 'autoridad_' + autor.replace(', ','_') + '.xml'
-
-print 'Obteniendo XML de ' + autor
 config = {
-    'op': 'find',
     'code': 'wau',
-    'request': autor,
     'base': 'BNC01'
 }
+request = FindRequest(base_url=url, config=config)
+metadata = request.find(name=author)
 
-res = _r.get( url, params = config )
-res.raise_for_status()
+# no_records = d['no_records']
+no_entries = metadata['no_entries']
+author_id = metadata['set_number']
 
-xml = _et.fromstring( res.content )
-d = {}
-# TODO usar clase Request
-for child in xml:
-    if child.tag in ( 'error', 'session-id' ): continue
-    #set_number, no_records, no_entries
-    d[child.tag] = int(child.text)
-
-#si no hay coincidencias con find, se abandona el script
-if len(d) == 0:
-    print('Error: Busqueda infructuosa.')
-    sys.exit()
-print d
-
-# por lo que se ve, hay que usar el numero de entries
-#no_records = d['no_records']
-no_entries = d['no_entries']
-set_number = d['set_number']
-i = 1
-bad_words = []
-# resetear el output en caso de existir
-open(output, 'w').close()
-newfile = open(output, 'a');
-# realizar ciclo que recupere de 100 en 100 los registros
-while i <= no_entries+99:
-    config = {
-        'op': 'present',
-        'set_entry': str(i)+'-'+str(i+99),
-        'set_number': set_number
-    }
-    res = _r.get( url, params = config )
-    res.raise_for_status()
-    text_data = res.content
-    #text_file = open('temporal.xml', 'w')
-    #text_file.write(res.content)
-    #text_file.close()
-    # filtrar palabras no deseadas para no ser incluidas en el xml
-    if i == 1:
-        bad_words = ['<session-id>', '</present>', '<error>']
-    else:
-        bad_words = ['<?xml', '<present>', '<session-id>', '</present>', '<error>']
-    # escribir en el output desde el archivo temporal
-    for line in text_data.splitlines():
-        if line != ' ' and not any(bad_word in line for bad_word in bad_words):
-            newfile.write(line+'\n')
-    '''
-    with open('temporal.xml','r') as oldfile, open(output, 'a') as newfile:
-        for line in oldfile:
-            if not any(bad_word in line for bad_word in bad_words):
-                newfile.write(line)
-    '''
-    i += 100
-# escribir ultima linea del output
-newfile.write('</present>')
-newfile.close()
-
-'''
-Intento de prettificar
-new_xml = xml.dom.minidom.parse(output) # or xml.dom.minidom.parseString(xml_string)
-pretty_xml = xml.toprettyxml()
-text_file = open(output + '_pretty.xml', 'w')
-text_file.write()
-text_file.close()
-'''
-
-print('Listo!')
+request = PresentRequest(base_url=url, config=config, no_entries=no_entries)
+# Here we should create a new rdf_graph with some library or by ourselves
+# For example: an authority graph class for these
+rdf_graph = ''  # authority_graph
+while request.remain_data:
+    author_data = request.get_chunk(author_id=author_id)
+    # Somehow add the data to the graph
+    rdf_graph.add(author_data)
+    # If the data crosses some threshold, flush it
+    if rdf_graph.fullMemory():
+        rdf_graph.writetoTTL()
+# Last flush
+rdf_graph.writetoTTL()
 
--- request/present_request.py
+++ request/present_request.py
@@ -1,21 +1,26 @@
 from request import Request
-# import xml.etree.ElementTree as _et
 
 
 class PresentRequest(Request):
-    name = ""
-    set_number = -1
-    no_records = -1
-    no_entries = -1
-
-    def __init__(self, base_url="", config=""):
+    def __init__(self, base_url='', config='', no_entries=0):
         super(PresentRequest, self).__init__(base_url, config)
-        self.config['op'] = 'find'
+        self.config['op'] = 'present'
+        self.no_entries = no_entries
+        self.chunk_size = 100
+        self.current_entry = 1
+        self.next_entry = max(no_entries, self.chunk_size)
+        self.config['set_entries'] = str(self.current_entry) + '-' + str(self.current_entry)
 
-    def get_chunk(self, name=None, base_url=None, config=None):
+
+    def get_chunk(self, author_id):
+        self.config['set_number'] = author_id
         # tbd
-        return
+        # TODO: get the stuff
+        self.current_entry += self.chunk_size
+        self.next_entry += self.chunk_size
 
+    def remain_data(self):
+        return self.current_entry == self.no_entries
 
 '''
 entregar name -> buscar -> obtener metadatos
@@ -23,4 +28,44 @@ entregar name -> buscar -> obtener metadatos
 -> saber cuantos datos tengo que sacar ->
 iterar hasta que no queden pedazos de informacion
 -> saber escribir cada pedazo obtenido (los chunk retornar xmls)
+'''
+# TODO: this should go somehow to the present request
+    # config = {
+    #    'op': 'present',
+    #    'set_entry': str(i)+'-'+str(i+99),
+    #    'set_number': set_number
+    # }
+    # res = _r.get( url, params = config )
+    # res.raise_for_status()
+    # text_data = res.content
+    # text_file = open('temporal.xml', 'w')
+    # text_file.write(res.content)
+    # text_file.close()
+    # filtrar palabras no deseadas para no ser incluidas en el xml
+    # if i == 1:
+    #    bad_words = ['<session-id>', '</present>', '<error>']
+    # else:
+    #    bad_words = ['<?xml', '<present>', '<session-id>', '</present>', '<error>']
+    # escribir en el output desde el archivo temporal
+    # for line in text_data.splitlines():
+    #    if line != ' ' and not any(bad_word in line for bad_word in bad_words):
+    #        newfile.write(line+'\n')
+    '''
+    with open('temporal.xml','r') as oldfile, open(output, 'a') as newfile:
+        for line in oldfile:
+            if not any(bad_word in line for bad_word in bad_words):
+                newfile.write(line)
+    '''
+    #i += 100
+# escribir ultima linea del output
+# newfile.write('</present>')
+# newfile.close()
+
+'''
+Intento de prettificar
+new_xml = xml.dom.minidom.parse(output) # or xml.dom.minidom.parseString(xml_string)
+pretty_xml = xml.toprettyxml()
+text_file = open(output + '_pretty.xml', 'w')
+text_file.write()
+text_file.close()
 '''
