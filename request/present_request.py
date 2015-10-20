6315ebd5cd4d8e16cbc3bb1f92cacb7d1e95e828
fernando <nanocatdemen@gmail.com>
2015-10-20 16:42:24 -0300

    Revert "get OK with correct use of variables"
    This reverts commit 86987c3b9a386bbe68789490cf8488d9aafc7767.

--- request/present_request.py
+++ request/present_request.py
@@ -9,7 +9,7 @@ class PresentRequest(Request):
         self.no_entries = no_entries
         self.chunk_size = 100
         self.current_entry = 1
-        self.config['set_entry'] = str(self.current_entry) + '-' + str(self.current_entry + self.chunk_size - 1)
+        self.config['set_entry'] = str(self.current_entry) + '-' + str(self.current_entry+99)
 
 
     def get_chunk(self, author_id):
@@ -17,6 +17,7 @@ class PresentRequest(Request):
         self.config['set_number'] = author_id
         self.config['set_entry'] = str(self.current_entry) + '-' + str(self.current_entry+99)        
         print self.config
+        # TODO: no funciona esta wea, me tira un xml todo mula
         response = self.send()
         '''
         xml = _Et.fromstring(response.content)
@@ -28,8 +29,8 @@ class PresentRequest(Request):
         self.current_entry = min(self.no_entries, self.current_entry + self.chunk_size)
         self.next_entry += self.chunk_size
         '''
-        self.current_entry = min(self.no_entries, self.current_entry + self.chunk_size)
+        self.current_entry += 100
         return response.content
 
     def remain_data(self):
-        return self.current_entry < self.no_entries
+        return self.current_entry <= self.no_entries+99
