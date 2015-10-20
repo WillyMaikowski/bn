13009f70b3497bb811ed4e0a8957f25932b7680f
Carlo Dettoni <cpdettoni@gmail.com>
2015-10-20 11:52:10 -0300

    present_request defines memory use

--- get.py
+++ get.py
@@ -2,7 +2,7 @@ from request.find_request import FindRequest
 from request.present_request import PresentRequest
 
 # TODO: Como lo haremos para todas las autoridades? encapsulamos esto en una clase person_etl?
-author = "Cortazar, Julio"  # sys.argv[1]
+author = "Mistral, Gabriela"  # sys.argv[1]
 output = 'authority_' + author.replace(', ','_') + '.xml'
 
 url = 'http://www.bncatalogo.cl/X'
@@ -21,19 +21,22 @@ request = PresentRequest(base_url=url, no_entries=no_entries)
 # resetear el output en caso de existir
 open(output, 'w').close()
 newfile = open(output, 'a');
+author_data = ''
 # realizar ciclo que recupere de 100 en 100 los registros
 while request.remain_data():
     # se obtiene un trozo del XML
-    author_data = request.get_chunk(author_id=author_id)
-    # filtrar palabras no deseadas para no ser incluidas en el xml
-    if request.current_entry == 1:
-        bad_words = ['<session-id>', '</present>', '<error>']
-    else:
-        bad_words = ['<?xml', '<present>', '<session-id>', '</present>', '<error>']
-    # escribir en el output desde author_data
-    for line in author_data.splitlines():
-        if line != ' ' and not any(bad_word in line for bad_word in bad_words):
-            newfile.write(line+'\n')
+    author_data += request.get_chunk(author_id=author_id)
+    if(request.current_entry % request.max_entries == 1 or request.current_entry >= request.no_entries):
+        # filtrar palabras no deseadas para no ser incluidas en el xml
+        if request.current_entry == 1:
+            bad_words = ['<session-id>', '</present>', '<error>']
+        else:
+            bad_words = ['<?xml', '<present>', '<session-id>', '</present>', '<error>']
+        # escribir en el output desde author_data
+        for line in author_data.splitlines():
+            if line != ' ' and not any(bad_word in line for bad_word in bad_words):
+                newfile.write(line+'\n')
+        author_data = ''
 # Last flush
 newfile.write('</present>')
 newfile.close()
