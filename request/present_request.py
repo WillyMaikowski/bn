3000891c2af13caa8c3328377851b83500e212fc
fernando <nanocatdemen@gmail.com>
2015-10-20 16:42:20 -0300

    Revert "present_request defines memory use"
    This reverts commit 13009f70b3497bb811ed4e0a8957f25932b7680f.

--- request/present_request.py
+++ request/present_request.py
@@ -9,7 +9,6 @@ class PresentRequest(Request):
         self.no_entries = no_entries
         self.chunk_size = 100
         self.current_entry = 1
-        self.max_entries = self.chunk_size * 4
         self.config['set_entry'] = str(self.current_entry) + '-' + str(self.current_entry + self.chunk_size - 1)
 
 
@@ -29,7 +28,7 @@ class PresentRequest(Request):
         self.current_entry = min(self.no_entries, self.current_entry + self.chunk_size)
         self.next_entry += self.chunk_size
         '''
-        self.current_entry = self.current_entry + self.chunk_size
+        self.current_entry = min(self.no_entries, self.current_entry + self.chunk_size)
         return response.content
 
     def remain_data(self):
