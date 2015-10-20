a59d037d3d139b6138247c4a4767b36b405adb29
Carlo Dettoni <cpdettoni@gmail.com>
2015-10-20 10:12:10 -0300

    get with classes OK

--- get.py
+++ get.py
@@ -2,32 +2,40 @@ from request.find_request import FindRequest
 from request.present_request import PresentRequest
 
 # TODO: Como lo haremos para todas las autoridades? encapsulamos esto en una clase person_etl?
-author = 'Cortazar, Julio'  # sys.argv[1]
+author = "Neruda, Pablo"  # sys.argv[1]
+output = 'authority_' + author.replace(', ','_') + '.xml'
 
 url = 'http://www.bncatalogo.cl/X'
-
-request = FindRequest(base_url=url)
+config = {
+    'code': 'wau',
+    'base': 'BNC01'
+}
+request = FindRequest(base_url=url, config=config)
 metadata = request.find(name=author)
 
-print 'Metadata: ' + str(metadata)
-
-# no_records = d['no_records']
 no_entries = metadata['no_entries']
 author_id = metadata['set_number']
-
-request = PresentRequest(base_url=url, no_entries=no_entries, author_id=author_id)
-# Here we should create a new rdf_graph with some library or by ourselves
-# For example: an authority graph class for these
-rdf_graph = ''  # authority_graph
-
+bad_words = []
+# se setean los parametros para hacer el request
+request = PresentRequest(base_url=url, no_entries=no_entries)
+# resetear el output en caso de existir
+open(output, 'w').close()
+newfile = open(output, 'a');
+# realizar ciclo que recupere de 100 en 100 los registros
 while request.remain_data():
-    author_data = request.get_chunk()
-    # Somehow add the data to the graph
-    # rdf_graph.add(author_data)
-    # If the data crosses some threshold, flush it
-    # IMPORTANT NOTE: probably a single authority cant cause run out the memory
-    # maybe this is needed in an upper level
-    # if rdf_graph.fullMemory():
-    #    rdf_graph.writetoTTL()
+    # se obtiene un trozo del XML
+    author_data = request.get_chunk(author_id=author_id)
+    # filtrar palabras no deseadas para no ser incluidas en el xml
+    if request.current_entry == 1:
+        bad_words = ['<session-id>', '</present>', '<error>']
+    else:
+        bad_words = ['<?xml', '<present>', '<session-id>', '</present>', '<error>']
+    # escribir en el output desde author_data
+    for line in author_data.splitlines():
+        if line != ' ' and not any(bad_word in line for bad_word in bad_words):
+            newfile.write(line+'\n')
 # Last flush
-# rdf_graph.writetoTTL()
+newfile.write('</present>')
+newfile.close()
+#rdf_graph.writetoTTL()
+
