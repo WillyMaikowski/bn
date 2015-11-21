f1a631648a65255b7427e7ceae8ac3e9dede2cb1
WillyMaikowski <willy.maikowski@gmail.com>
2015-11-21 18:53:42 -0300

    Minimas mejoras

--- authors.py
+++ authors.py
@@ -1,20 +1,21 @@
-# -*- coding: utf-8 -*
+# -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
 import json
 
-class Authors(object):
-    def __init__(self):
-        self.names = {}
-        
-    def addAuthorsFromXML(self, filename):
-        for event, elem in ET.iterparse( filename, events=( 'start', 'end', 'start-ns', 'end-ns' ) ):
-            if event != 'end': continue
-            if elem.tag == 'property-value' and elem.get('pnid') == '551':
-                name = elem.get('name')
-                self.names.update({name:0})
+class Authors( object ):
+	def __init__( self ):
+		self.names = {}
 
-    def showAuthors(self):
-        return json.dumps(self.names, ensure_ascii=False)
+	def addFromXML( self, filename ):
+		for event, elem in ET.iterparse( filename, events=( 'start', 'end', 'start-ns', 'end-ns' ) ):
+			if event != 'end': continue
+			if elem.tag == 'property-value' and elem.get('pnid') == '551':
+				name = elem.get('name')
+				if name not in self.names: self.names[name] = 0
+				self.names[name] += 1
 
-    def countAuthors(self):
-        return len(self.names)
+	def __str__( self ):
+		return str( self.names.keys() )
+
+	def __len__( self ):
+		return len( self.names )
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from ETL.E.G2.request.find_request import FindRequest
 from authors import Authors
+import os
 
 authors = Authors()
-authors.addAuthorsFromXML('Datos-MCH/000 - 999.xml')
+# authors.addFromXML('mch/000 - 999.xml')
 # Dejo comentada esta parte, pues con todos los archivos que hay, el proceso es algo lento.
-'''
-i = 1000
-while i < 27000:
-    authors.addAuthorsFromXML('Datos-MCH/' + str(i) + ' - ' + str(i+999) + '.xml')
-    i += 1000
-    print i
-authors.addAuthorsFromXML('Datos-MCH/28000 - 28371.xml')
-'''
-print authors.showAuthors()
-print '*** Numero de autores ***'
-print authors.countAuthors()
+for dirpath, dnames, fnames in os.walk("./mch/"):
+    for f in fnames:
+        if f.endswith(".xml"):
+            print os.path.join( dirpath, f )
+            authors.addFromXML( os.path.join( dirpath, f ) )
 
+#print authors
+#print '*** Numero de autores ***'
+print len( authors )
+
+'''
 url = 'http://www.bncatalogo.cl/X'
 request = FindRequest(base_url=url)
 request = request.find(name='Cortazar, Julio')
 print request
+'''
