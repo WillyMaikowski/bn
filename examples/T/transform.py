d86cf0de7123954f4b155d61eb0c241141cb0a8d
fernando <nanocatdemen@gmail.com>
2015-12-07 12:38:23 -0300

    Add config as json

--- examples/T/transform.py
+++ examples/T/transform.py
@@ -1,8 +1,10 @@
 from ETL.T.transformer import Transformer
 import xml.etree.ElementTree as ET
+import json
 
 xml = ET.parse("sample.xml")
-config = {"book/author": {"prefix": "foaf", "property": "name"}, "book/title": {"prefix": "foaf", "property": "primaryTopic"}}
+with open("config.json", "r") as fp:
+    config = json.load(fp)
 t = Transformer(config, "http://example.com/", "book/author", {"foaf": "http://xmlns.com/foaf/0.1/"})
 g = t.transform(xml)
 
