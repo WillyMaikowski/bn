4880d90384f3fb2dfbdb1bc7acc81f6619e639ef
Carlo Dettoni <cpdettoni@gmail.com>
2015-12-12 13:00:26 -0300

    main2.py contenido en main.py

--- main.py
+++ main.py
@@ -13,18 +13,16 @@ authors = Authors()
 authors.add_from_xml("data/000 - 999.xml")
 
 aleph_data = ET.fromstring("<aleph></aleph>")
-
 cnt = 0
-n_authors = len(authors)
+total = 20
 for author in authors:
     cnt += 1
-    if cnt == 41:
+    if cnt == total:
         break
-    print str(cnt) + "/40"
+    print "Loading authors: " + str(100*cnt/total) + "%"
     url = 'http://www.bncatalogo.cl/X'
     request = FindRequest(base_url=url)
     metadata = request.find(name=author)
-
     if len(metadata.keys()) == 0:
         logging.warning("Author '" + author + "' not found.")
         continue
@@ -36,62 +34,124 @@ for author in authors:
     author_data = []
     while request.remain_data():
         author_data.append(request.get_chunk())
-
     for elem in author_data:
         aleph_data.append(elem)
-
+print "Authors 100% loaded."
 aleph_xml = ET.ElementTree()
 aleph_xml._setroot(aleph_data)
 
 def preprocess_aleph(xml):
-    name_xpath = "present/record/metadata/oai_marc/varfield[@id='100']/subfield[@label='a']"
-    title_xpath = "present/record/metadata/oai_marc/varfield[@id='245']/subfield[@label='a']"
-    nick_xpath = "present/record/metadata/oai_marc/varfield[@id='245']/subfield[@label='c']"
-    alternative_xpath = "present/record/metadata/oai_marc/varfield[@id='246']/subfield[@label='a']"
-    references_xpath = "present/record/metadata/oai_marc/varfield[@id='600']/subfield[@label='a']"
-    contributor_xpath = "present/record/metadata/oai_marc/varfield[@id='700']/subfield[@label='a']"
+    base_xpath = "present/record/metadata/oai_marc/"
+    name_xpath = base_xpath + "varfield[@id='100']/subfield[@label='a']"
+    org_xpath = base_xpath + "varfield[@id='110']/subfield[@label='a']"
+    title_xpath = base_xpath + "varfield[@id='245']/subfield[@label='a']"
+    nick_xpath = base_xpath + "varfield[@id='245']/subfield[@label='c']"
+    alternative_xpath = base_xpath + "varfield[@id='246']/subfield[@label='a']"
+    references_xpath = base_xpath + "varfield[@id='600']/subfield[@label='a']"
+    contributor_xpath = base_xpath + "varfield[@id='700']/subfield[@label='a']"
+    br1_xpath = base_xpath + "varfield[@id='856']/subfield[@label='a']"
+    br2_xpath = base_xpath + "varfield[@id='856']/subfield[@label='d']"
+    br3_xpath = base_xpath + "varfield[@id='856']/subfield[@label='f']"
+    br_uri1 = xml.findall(br1_xpath)
+    br_uri2 = xml.findall(br2_xpath)
+    br_uri3 = xml.findall(br3_xpath)
+    br_uri_largo = len(br_uri1)
+    for i in range(0, br_uri_largo-1):
+        br_uri1[i].text = br_uri1[i].text + br_uri2[i].text + br_uri3[i].text
+        br_uri3[i].text = br_uri3[i].text.replace(".","_")
     for elem in xml.findall(name_xpath):
+        elem.text = elem.text.replace(".","")
         if elem.text[-1] == ",":
+            elem.text = elem.text[0:len(elem.text)-1]
+        name_parts = elem.text.split(", ")
+        if len(name_parts) == 2:
+            elem.text = name_parts[1] + " " + name_parts[0]
+    for elem in xml.findall(org_xpath):
+        if elem.text[-1] == ".":
             elem.text = elem.text[0:len(elem.text)-1]
     for elem in xml.findall(title_xpath):
         if elem.text[-1] == "/" or elem.text[-1] == ":":
             elem.text = elem.text[0:len(elem.text)-1]
         elem.text = elem.text.strip()
     for elem in xml.findall(nick_xpath):
+        elem.text = elem.text.replace(".","")
         if elem.text[-1] == ".":
             elem.text = elem.text[0:len(elem.text)-1]
     # for elem in xml.findall(alternative_xpath):
     for elem in xml.findall(references_xpath):
+        elem.text = elem.text.replace(".","")
         if elem.text[-1] == ",":
             elem.text = elem.text[0:len(elem.text)-1]
+        name_parts = elem.text.split(", ")
+        if len(name_parts) == 2:
+            elem.text = name_parts[1] + " " + name_parts[0]
     for elem in xml.findall(contributor_xpath):
+        elem.text = elem.text.replace(".","")
         if elem.text[-1] == ",":
             elem.text = elem.text[0:len(elem.text)-1]
+        name_parts = elem.text.split(", ")
+        if len(name_parts) == 2:
+            elem.text = name_parts[1] + " " + name_parts[0]
     return xml
 
+print "Preprocessing Aleph..."
 aleph_xml = preprocess_aleph(aleph_xml)
+print "Preprocess completed."
 
-
-print "---ALEPH WORK---"
-with open("config/config_aleph_work.json", "r") as fp:
-    config = json.load(fp)
-t = Transformer(config, "http://example.com/obra/", "varfield[@id='245']/subfield[@label='a']",
-                {"dct": "http://purl.org/dc/terms/", "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-                 "ex": "http://example.com/"}
-                )
+### ET FROM ALEPH ###
+# Person Output
+with open("config/config_aleph_person.json", "r") as fp:
+   config = json.load(fp)
+t = Transformer(config, "http://datos.bn.cl/recurso/persona/",
+                "varfield[@id='100']/subfield[@label='a']",
+                {"foaf": "http://xmlns.com/foaf/0.1/", "rdfs": "http://www.w3.org/2000/01/rdf-schema#", "lib": "http://datos.bn.cl"}
+               )
 g = t.transform(aleph_xml)
-g.serialize(destination="output/output_aleph_work.ttl", format='turtle')
-
-print "---ALEPH AUTHORITY---"
-with open("config/config_aleph_authority.json", "r") as fp:
-    config = json.load(fp)
-t = Transformer(config, "http://example.com/autoridad/", "varfield[@id='100']/subfield[@label='a']",
-                {"foaf": "http://xmlns.com/foaf/0.1/", "dct": "http://purl.org/dc/terms/",
-                 "rdfs": "http://www.w3.org/2000/01/rdf-schema#", "ex": "http://example.com/"}
-                )
+g.serialize(destination="output/output_aleph_person.ttl", format='turtle')
+print "Person loaded to TTL."
+# Organization Output
+with open("config/config_aleph_organization.json", "r") as fp:
+   config = json.load(fp)
+t = Transformer(config, "http://datos.bn.cl/recurso/institucion/",
+                "varfield[@id='110']/subfield[@label='a']",
+                {"foaf": "http://xmlns.com/foaf/0.1/", "rdfs": "http://www.w3.org/2000/01/rdf-schema#", "lib": "http://datos.bn.cl/"}
+               )
 g = t.transform(aleph_xml)
-g.serialize(destination="output/output_aleph_authority.ttl", format='turtle')
+g.serialize(destination="output/output_aleph_organization.ttl", format='turtle')
+print "Organization loaded to TTL."
+# CreativeWork Output
+with open("config/config_aleph_creativework.json", "r") as fp:
+   config = json.load(fp)
+t = Transformer(config, "http://datos.bn.cl/recurso/obra/",
+                "varfield[@id='245']/subfield[@label='a']",
+                {"foaf": "http://xmlns.com/foaf/0.1/", "rdfs": "http://www.w3.org/2000/01/rdf-schema#", "lib": "http://datos.bn.cl/", "dct": "http://purl.org/dc/terms/"}
+               )
+g = t.transform(aleph_xml)
+g.serialize(destination="output/output_aleph_creativework.ttl", format='turtle')
+print "CreativeWork loaded to TTL."
+# Collection Output
+with open("config/config_aleph_collection.json", "r") as fp:
+   config = json.load(fp)
+t = Transformer(config, "http://datos.bn.cl/recurso/tema/",
+                "varfield[@id='650']/subfield[@label='a']",
+                {"rdfs": "http://www.w3.org/2000/01/rdf-schema#", "lib": "http://datos.bn.cl/", "dct": "http://purl.org/dc/terms/"}
+               )
+g = t.transform(aleph_xml)
+g.serialize(destination="output/output_aleph_collection.ttl", format='turtle')
+print "Collection loaded to TTL."
+# BibliographicResource Output
+with open("config/config_aleph_br.json", "r") as fp:
+   config = json.load(fp)
+t = Transformer(config, "http://datos.bn.cl/recurso/material_digital/",
+                "varfield[@id='856']/subfield[@label='f']",
+                {"rdfs": "http://www.w3.org/2000/01/rdf-schema#", "lib": "http://datos.bn.cl/", "dct": "http://purl.org/dc/terms/"}
+               )
+g = t.transform(aleph_xml)
+g.serialize(destination="output/output_aleph_br.ttl", format='turtle')
+print "BibliographicResource loaded to TTL."
 
+### ET FROM MCH ###
+'''
 print "---MCH AUTHORITY---"
 xml = ET.parse("data/000 - 999.xml")
 with open("config/config_mch_authority.json", "r") as fp:
@@ -146,3 +206,4 @@ t = Transformer(config, "http://example.com/tema/", "properties/property/propert
                 )
 g = t.transform(xml)
 g.serialize(destination="output/output_mch_tema.ttl", format='turtle')
+'''
